1bd3d54f3635a0f5e62b6f1017889fdee3b18a1b
毓鎂 <persimmon823@gmail.com>
2020-08-06 11:30:16 +0800

    fix_1
    change the input of board and delete push_tag

--- Final_PTT_Crawler.py
+++ Final_PTT_Crawler.py
@@ -18,10 +18,7 @@ import numpy as np
 def get_web_page(url):
     time.sleep(0.1)
                                     #resp改成responce
-    responce = requests.get(
-        url=url,
-#
-    )
+    responce = requests.get(url=url)
     if responce.status_code != 200:
         print('Invalid url:', responce.url)
         return None
@@ -29,7 +26,7 @@ def get_web_page(url):
         return responce.text
 
 #############################################
-#          對PTT網頁進行資料擷取             #
+#          對PTT網頁進行資料擷取              #
 #############################################
 def get_data(dom):                            #搜索dom節點
     soup = BeautifulSoup(dom , 'html.parser')
@@ -38,7 +35,7 @@ def get_data(dom):                            #搜索dom節點
     return article, push_tag
 
 #############################################
-#          		讀取文章網址         #
+#          		讀取文章網址            	 #
 ##################a##########################
 def get_article_url(text):
     url = []
@@ -56,7 +53,7 @@ def get_article_url(text):
 #    	讀取看板頁面(沒有加搜尋字眼時使用)     #
 ##################a##########################
 def getNext(url):
-    urls = get_web_page(url)                      #urls
+    urls = get_web_page(url)
     soup = BeautifulSoup(urls, 'html.parser')
     div = soup.find_all('a','btn wide')
     for i in div:
@@ -68,7 +65,6 @@ def getNext(url):
 #          			畫圖				     #
 ##################a##########################
 ##################圓餅圖######################
-
 def DrawPie(font, labels_list, percent_list, title):				#labels_list: 圓餅圖的字    #percent_list: 圓餅圖各項的比例
 	labels, sizes = [], []											
 	plt.title(title, fontproperties = font)
@@ -85,12 +81,11 @@ def DrawPie(font, labels_list, percent_list, title):				#labels_list: 圓餅圖�
 ##################直方圖######################	
 def DrawBar(font, sem_list, bar_list, title):						#sem_list: 直方圖的每條上的字   #bar_list: 直方圖的長度
 	plt.title(title, fontproperties = font)
-	y_pos = np.arange(1)                                                            #y_pos產生y軸座標序列
-	plt.xticks(y_pos + .3/2, (''), fontproperties = font)                           #xticks設定x軸刻度標籤
+	y_pos = np.arange(1)
+	plt.xticks(y_pos + .3/2, (''), fontproperties = font)
 	for i in range(len(sem_list)):
 		plt.bar(y_pos + 0.25*i , bar_list[i], 0.2, alpha=.5, label = sem_list[i])
 	plt.legend(loc = "upper right", prop = font)
-
 	
 #############################################
 #           主程式:進行資料分析              #
@@ -101,32 +96,42 @@ if __name__ == '__main__':
     #3 = '振興'
     #4 = '三倍'
     KEY = 1
-    Board = str(input("請輸入想要搜尋的版(Ex:creditcard)  :  "))
+    datasize = eval(input("請輸入欲分析的詞彙個數  :  "))
+    print('''省錢: Lifeismoney/CPBL: Elephants/籃球: NBA,
+遊戲: LOL/Hate: HatePolitics/婚姻: marriage,
+車車: car/資訊: MobileComm/工作: Tech_Job,
+聊天: WomenTalk/心情: Boy-Girl/家庭: BabyMother,
+硬體: PC_Shopping/娛樂: joke/主機: PlayStation,
+韓國: KoreaStar/聯誼: AllTogether/理財: creditcard,
+高雄: Kaohsiung/台南: Tainan/CPBL: Lions,
+主機: NSwitch/CPBL:  Guardians/韓劇: KoreaDrama,
+綜藝: KR_Entertain/手遊: PCReDive/資訊: CVS,
+台中: TaichungBun/系統: iOS/美容: MakeUp''')
+    Board = input("請輸入欲查詢看板 : ")
     PTT_URL = 'https://www.ptt.cc/bbs/' if KEY == 1 else 'https://www.ptt.cc/bbs/' + Board + '/index.html'
     page_num = 10
-    datasize = eval(input("請輸入欲分析的詞彙個數  :  "))  
-    new_sum_sem_list = [0]*datazise
+    new_sum_sem_list = [0]*datasize
     urls = []
     semantic_list = []			#存放輸入的關鍵字
     #這個lis的命名意義???
-    
-    for num_word in range(datasize):                      #i改為num_word
+
+    for num_word in range(datasize):
         semantic_in = input("請輸入第"+str(num_word+1)+"個關鍵字  :  ")			#改變你想要找的關鍵字
         Search = semantic_in
         semantic_list.append(semantic_in)
-    for critic_word in range(0,datasize):                        #q改為critic_word
+    for critic_word in range(0,datasize):
         KEY = 1#有沒有加入搜尋字眼 1:有 0:沒有
         if critic_word == 0:
-            Board = 'creditcard'	    	#選取PTT看板	!!!!!!(凡是設有內容分級規定處理，即不能直接進入看板者，EX.八卦版...等會沒辦法爬)!!!!!
+            # Board = 'creditcard'	    	#選取PTT看板	!!!!!!(凡是設有內容分級規定處理，即不能直接進入看板者，EX.八卦版...等會沒辦法爬)!!!!!
             Search = '振興'   		#加入搜尋特定字眼的文章 EX.在「省錢」/「理財」版找尋標題有含'振興券/卷'or'三倍券/卷'的文章
         elif critic_word == 1:
-            Board = 'creditcard'
+            # Board = 'creditcard'
             Search = '三倍'
         elif critic_word == 2:
-            Board = 'Lifeismoney'
+            # Board = 'Lifeismoney'
             Search = '振興'
         elif critic_word == 3:
-            Board = 'Lifeismoney'
+            # Board = 'Lifeismoney'
             Search = '三倍'
         ############################################################
         '''
@@ -134,15 +139,15 @@ if __name__ == '__main__':
         '''
         ############################################################
         #輸入關鍵字
-
+ 
         '''
         semantic_list = []			#存放輸入的關鍵字
-        for i in range(datasize):
+        for i in range(datazise):
             semantic_in = input("請輸入第"+str(i+1)+"個關鍵字  :  ")			#改變你想要找的關鍵字
             semantic_list.append(semantic_in)
         '''
         ############################################################
-        articles, push_tags = [], []		#articles: ptt文章所有內容   #push_tags: 推噓文資訊
+        articles = []	#articles: ptt文章所有內容 
         for page in range(page_num):	#取得PTT頁面資訊
             url_key = PTT_URL + Board + '/search?page=' + str(page+1) + '&q=' + Search
             url = url_key if KEY == 1 else PTT_URL if page == 0 else getNext(PTT_URL)
@@ -152,9 +157,9 @@ if __name__ == '__main__':
             for url in urls:
                 print(url)
                 text = get_web_page(url)
-                article, push_tag = get_data(text)
+                article = get_data(text)
                 articles.append(article)
-                push_tags.append(push_tag)
+                
         ############################################################
         #計算關鍵字出現次數，以及關鍵字出現的文章其推噓文數量
         sum_sem_list = []			#該關鍵字出現總數
